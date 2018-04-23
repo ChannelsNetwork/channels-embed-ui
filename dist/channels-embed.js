15d1dce768ec961b20e65a57ef621b658118ee0d
Preet Shihn <preetshihn@gmail.com>
2018-04-23 14:16:37 -0700

    .

--- dist/channels-embed.js
+++ dist/channels-embed.js
@@ -2005,7 +2005,6 @@ var channels = (function (exports) {
       <style>
         :host {
           display: block;
-          cursor: pointer;
         }
       
         .container {
@@ -2021,6 +2020,11 @@ var channels = (function (exports) {
           background: #293C41;
           border-radius: 30px 0 0 30px;
           box-shadow: 0 3px 4px 0 rgba(0, 0, 0, 0.14), 0 1px 8px 0 rgba(0, 0, 0, 0.12), 0 3px 3px -2px rgba(0, 0, 0, 0.4);
+          cursor: pointer;
+        }
+      
+        .container:hover #tip {
+          display: block;
         }
       
         .logo {
@@ -2035,10 +2039,37 @@ var channels = (function (exports) {
           height: 60px;
           box-sizing: border-box;
         }
+      
+        .hidden {
+          display: none;
+        }
+      
+        #tip {
+          position: absolute;
+          top: 100%;
+          right: 0px;
+          width: 280px;
+          padding: 10px;
+          margin-top: 10px;
+          box-shadow: 0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.4);
+          font-family: sans-serif;
+          font-weight: 300;
+          font-size: 15px;
+          letter-spacing: 0.02em;
+          line-height: 1.3em;
+          color: #fff;
+          background: rgba(0, 0, 0, 0.85);
+          cursor: initial;
+          display: none;
+        }
       </style>
       <div class="container">
         <img class="logo" src="https://channels.cc/s/images/logos/logo_200.png">
         <div class="balanceBar"></div>
+        <div id="tip">
+          Channels is a micropayment system for content creators. Readers pay publishers using their Channels credit, which they can
+          earn by watching sponsered content.
+        </div>
       </div>
     `;
         }
--- src/components/tab.ts
+++ src/components/tab.ts
@@ -11,7 +11,6 @@ export class ChannelsTab extends LitElement {
       <style>
         :host {
           display: block;
-          cursor: pointer;
         }
       
         .container {
@@ -27,6 +26,11 @@ export class ChannelsTab extends LitElement {
           background: #293C41;
           border-radius: 30px 0 0 30px;
           box-shadow: 0 3px 4px 0 rgba(0, 0, 0, 0.14), 0 1px 8px 0 rgba(0, 0, 0, 0.12), 0 3px 3px -2px rgba(0, 0, 0, 0.4);
+          cursor: pointer;
+        }
+      
+        .container:hover #tip {
+          display: block;
         }
       
         .logo {
@@ -41,10 +45,37 @@ export class ChannelsTab extends LitElement {
           height: 60px;
           box-sizing: border-box;
         }
+      
+        .hidden {
+          display: none;
+        }
+      
+        #tip {
+          position: absolute;
+          top: 100%;
+          right: 0px;
+          width: 280px;
+          padding: 10px;
+          margin-top: 10px;
+          box-shadow: 0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.4);
+          font-family: sans-serif;
+          font-weight: 300;
+          font-size: 15px;
+          letter-spacing: 0.02em;
+          line-height: 1.3em;
+          color: #fff;
+          background: rgba(0, 0, 0, 0.85);
+          cursor: initial;
+          display: none;
+        }
       </style>
       <div class="container">
         <img class="logo" src="https://channels.cc/s/images/logos/logo_200.png">
         <div class="balanceBar"></div>
+        <div id="tip">
+          Channels is a micropayment system for content creators. Readers pay publishers using their Channels credit, which they can
+          earn by watching sponsered content.
+        </div>
       </div>
     `;
   }
