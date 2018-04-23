87c7d09ef3c85539afa9ab5a3ac4da06b3f34737
Preet Shihn <preetshihn@gmail.com>
2018-04-23 11:34:16 -0700

    .

--- dist/channeles-embed-ui.js
+++
@@ -1,5 +0,0 @@
-import { ChannelsTab } from './components/channels-tab.js';
-export function setup() {
-    document.body.appendChild(new ChannelsTab());
-}
-//# sourceMappingURL=channeles-embed-ui.js.map
--- dist/components/channels-tab.d.ts
+++
@@ -1,6 +0,0 @@
-import { LitElement, TemplateResult, Properties } from '../../node_modules/lit-component/index.js';
-export declare class ChannelsTab extends LitElement {
-    static readonly properties: Properties;
-    label?: String;
-    template(): TemplateResult;
-}
